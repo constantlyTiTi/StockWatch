090c211d7a6a0c4e9b829e5ea9e0f83e26022616
ting <tli101@my.centennialcollege.ca>
2024-01-29 10:04:00 -0500

    update display

--- FrontEnd/app/page.tsx
+++ FrontEnd/app/page.tsx
@@ -1,32 +1,43 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { Typography, Grid, Box, Button } from "@mui/material";
 
-import { ThemeContext } from '@emotion/react'
-import Link from 'next/link'
-import { Typography } from '@mui/material'
 export default function Home() {
+  const router = useRouter()
 
-
+  const navigateToNextPage = () =>{
+    router.push(`/stock`)
+  }
   return (
-    <>
-    <Typography variant="h5" component="div">
-      This app is created to monitor the stock price in real market; 
-      Currently, it only supports 8 stocks due to the limitation of third party api which is used to retrieve the real time data.
-    </Typography>
-    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-      FrontEnd is built with Next.js and Node.js, it listens to the websocket channel to get the latest changes.
-    </Typography>
-    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-      BackEnd is built with spring boot and JPA, integrated scheduler to retrieve data from third party API, and distribute data via Apache Kafka. 
-      Data will be saved into database in kafka consumer for better performance, as well as sending data to websocket channel.
-    </Typography>
-
-    <Link href="/stock">
-    <Typography variant="h5" component="div">
-        Now click here and enjoy
-      </Typography>
-    </Link>
-    
-    </>
-
-    
-  )
+    <Box m={2}>
+      <Grid container spacing={3} mb={5} >
+        <Grid item xs={12}sx={{ flexWrap: "wrap" }}>
+          <Typography variant="h5" component="div">
+            This app is created to monitor the stock price in real market;
+            Currently, it only supports 8 stocks due to the limitation of third
+            party api which is used to retrieve the real time data.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ flexWrap: "wrap" }}>
+          <Typography variant="h5" component="div">
+            FrontEnd is built with Next.js and Node.js, it listens to the
+            websocket channel to get the latest changes.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ flexWrap: "wrap" }}>
+          <Typography variant="h5" component="div">
+            BackEnd is built with spring boot and JPA, integrated scheduler to
+            retrieve data from third party API, and distribute data via Apache
+            Kafka. Data will be saved into database in kafka consumer for better
+            performance, as well as sending data to websocket channel.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Button onClick={navigateToNextPage} variant="outlined">
+        <Typography variant="h5" component="div">
+          Now click here and enjoy
+        </Typography>
+      </Button>
+    </Box>
+  );
 }
